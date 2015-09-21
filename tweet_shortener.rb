199035fb9282342208cfc30027a3d6a1a8646e07
Chris Aporta <christopher.aporta@gmail.com>
2015-09-20 21:16:34 -0400

    complete lab

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,52 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "For" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  arr = tweet.split
+  arr.map! do |word|
+    if dictionary.keys.include?(word)
+      word = dictionary[word]
+    else
+      word
+    end
+  end
+  new_str = arr.join(" ")
+end
+
+def bulk_tweet_shortener(tweet_arr)
+  tweet_arr.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+    tweet.slice!(137..-1)
+    tweet << "..."
+  end
+  tweet
+end
